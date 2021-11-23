3bea0e04c694d85aa138c72766616a08f26ab227
Rus <de.mr44@gmail.com>
2021-11-23 23:37:39 +0500

    Added the first method to download images asynchronously

--- Multithreading-GCD7/ViewController.swift
+++ Multithreading-GCD7/ViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 
-// some different ways to async download images
+// some different ways to download images asynchronously
 
 final class ViewController: UIViewController {
     
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var downloadButton: UIButton!
+    
+    let imageURL = URL(string: "https://images.unsplash.com/photo-1637672531763-7b7a63b3269b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +24,20 @@ final class ViewController: UIViewController {
     }
     
     // first approach
-    private func downloadImage() {
-        
+    private func firstMethodToDownloadImage() {
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            if let data = try? Data(contentsOf: self.imageURL) {
+                DispatchQueue.main.async {
+                    self.myImageView.image = UIImage(data: data)
+                }
+            }
+        }
     }
     
     @IBAction func downloadButtonAction(_ sender: UIButton) {
-        
+        myImageView.image = nil
+        firstMethodToDownloadImage()
     }
 }
 
